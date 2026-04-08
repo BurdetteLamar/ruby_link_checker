39f595c20117803816902f57027a5dc634250eac
BurdetteLamar <burdettelamar@yahoo.com>
2026-04-08 21:38:39 +0100

    More tests

--- test/test_ruby_link_checker.rb
+++ test/test_ruby_link_checker.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
+require 'open3'
 require 'test_helper'
 
 class TestRubyLinkChecker < Minitest::Test
 
   def test_version
-    refute_nil ::RubyLinkChecker::VERSION
+    refute_nil RubyLinkChecker::VERSION
   end
 
-  # This is the default source for the link checker;  it should exist.
+  # The default source for the link checker should exist.
   def test_master_exists
     master_url = File.join(RubyLinkChecker::BASE_URL, 'master', '')
     uri = URI.parse(master_url)
@@ -17,21 +18,50 @@ class TestRubyLinkChecker < Minitest::Test
   end
 
   def test_option_help
-    options = %w[ --help ]
-    arguments = []
-    stdout_s, stderr_s, status = do_command(options, arguments)
-    usage_text = 'Usage: bin/ruby_link_checker [options]'
-    assert_match(usage_text, stdout_s)
-    assert_empty(stderr_s)
-    assert_equal(0, status.exitstatus)
+    command = Command.new(
+      self,
+      options: %w[ --help ],
+      exp_stdout: 'Usage: bin/ruby_link_checker [options]',
+    )
+    command.execute
   end
 
-  def do_command(options, arguments)
-    executable = 'bin/ruby_link_checker'
-    command = "ruby #{executable} #{options.join(' ')} #{arguments.join(' ')}"
-    require 'open3'
-    Open3.capture3(command)
+  # Class to execute a command.
+  class Command
+
+    attr_accessor :test,                                  # TestRubyLinkChecker object
+                  :options, :arguments,                   # CLI options and arguments.
+                  :exp_stdout, :exp_stderr, :exp_status,  # Expected output values.
+                  :act_stdout, :act_stderr, :act_status   # Actual output values.
+
+    def initialize(
+      test,
+      options: [],
+      arguments: [],
+      exp_stdout: '',
+      exp_stderr: '',
+      exp_status: 0)
+      self.test = test
+      self.options = options
+      self.arguments = arguments
+      self.exp_stdout = exp_stdout
+      self.exp_stderr = exp_stderr
+      self.exp_status = exp_status
+    end
+
+    EXECUTABLE_PATH = 'bin/ruby_link_checker'
+
+    # Execute the command, after which actual values are available.
+    def execute
+      command = "ruby #{EXECUTABLE_PATH} #{options.join(' ')} #{arguments.join(' ')}"
+      self.act_stdout, self.act_stderr, status = Open3.capture3(command)
+      test.assert_match(exp_stdout, act_stdout, 'stdout')
+      test.assert_match(exp_stderr, act_stderr, 'stderr')
+      self.act_status = status.exitstatus
+      test.assert_equal(exp_status, act_status, 'status')
+    end
   end
+
 end
 
 

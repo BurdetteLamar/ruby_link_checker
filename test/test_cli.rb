b1e0bacfe0c9cdbf3106b0b9ee22e14e766a3021
BurdetteLamar <burdettelamar@yahoo.com>
2026-04-10 20:57:19 +0100

    More tests

--- test/test_cli.rb
+++ test/test_cli.rb
@@ -24,6 +24,9 @@ class TestRubyLinkChecker < Minitest::Test
 
   # The tests for the options below use option --no-op
   # to cause the checker and its reporter to print options.
+  # The tests verify only that the options are passed correctly,
+  # not that the checker uses them well or wisely;
+  # that's a job for other tests.
 
   def test_option_no_op
     Command.new(
@@ -64,6 +67,45 @@ class TestRubyLinkChecker < Minitest::Test
       options: %w[ --no-op --verbosity=FOO ],
       exp_status: 1,
       exp_stderr: 'FOO',
+    ).execute
+  end
+
+  def test_option_legal
+    Command.new(
+      self,
+      options: %w[ --no-op ],
+      exp_stdout: 'legal: false'
+    ).execute
+    Command.new(
+      self,
+      options: %w[ --no-op --legal ],
+      exp_stdout: 'legal: true'
+    ).execute
+  end
+
+  def test_option_news
+    Command.new(
+      self,
+      options: %w[ --no-op ],
+      exp_stdout: 'news: false'
+    ).execute
+    Command.new(
+      self,
+      options: %w[ --no-op --news ],
+      exp_stdout: 'news: true'
+    ).execute
+  end
+
+  def test_option_github_lines
+    Command.new(
+      self,
+      options: %w[ --no-op ],
+      exp_stdout: 'github_lines: false'
+    ).execute
+    Command.new(
+      self,
+      options: %w[ --no-op --github-lines ],
+      exp_stdout: 'github_lines: true'
     ).execute
   end
 
